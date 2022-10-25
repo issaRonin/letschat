7a2ac5a923c298ed16a35f548136d68f28137e7e
issaRonin <101780577+issaRonin@users.noreply.github.com>
2022-10-24 19:55:39 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,10 +12,31 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+    Room_names = childKey;
+   //Start code
+ console.log("Room Name -" + Room_names);
+ row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#" + Room_names +"</div><hr>";
+  document.getElementById("output").innerHTML += row;
+ //End code
+   });});}
+getData();
   function addRoom()
 {
     user_name = document.getElementById("room_name").value;
     firebase.database().ref("/").child(user_name).update({
-        purpose:"adding user"
+        purpose:"adding room"
     });
+}
+function redirectToRoomName()
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html";
+}
+function logOut()
+{
+  localStorage.removeItem(user_name);
+  localStorage.removeItem(room_name);
+  window.location = "index.html";
 }
